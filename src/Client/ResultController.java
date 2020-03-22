8d6a0772f66c213daebd7c38b150cbd8d0315f0c
nventuri97 <n.venturi3@studenti.unipi.it>
2020-03-22 16:06:34 +0100

    Bug fix

--- src/Client/ResultController.java
+++ src/Client/ResultController.java
@@ -8,7 +8,10 @@ import javafx.event.ActionEvent;
 
 public class ResultController {
     private ClientGui clientGui;
-    private Label result;
+    private Label result1;
+    private Label result2;
+    private Label result3;
+    private Label result4;
     private Button home;
 
     public void setClientGui(ClientGui client){
@@ -16,13 +19,30 @@ public class ResultController {
     }
 
     public void setAnchor(Parent root){
-        result=(Label) root.lookup("#result");
-        result.setText("");
+        result1=(Label) root.lookup("#result1");
+        result1.setText("");
+        result2=(Label) root.lookup("#result2");
+        result2.setText("");
+        result3=(Label) root.lookup("#result3");
+        result3.setText("");
+        result4=(Label) root.lookup("#result4");
+        result4.setText("");
     }
 
     public void showResult(){
         String answer=clientGui.clientConnection.receiveResponse();
-        result.setText(answer);
+        String[] substr=answer.split("\\s+");
+        if(substr[1].equals("won")){
+            result1.setText(substr[0]+" "+substr[1]+". "+substr[5]+" "+substr[6]+" "+substr[7]+" "+substr[8]+" "+substr[9]);
+            result2.setText(substr[2]);
+            result3.setText(substr[3]);
+            result4.setText(substr[4]);
+        }else{
+            result1.setText(substr[0]+" "+substr[1]);
+            result2.setText(substr[2]);
+            result3.setText(substr[3]);
+            result4.setText(substr[4]);
+        }
     }
 
     @FXML
